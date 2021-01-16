92835aad211ba0968bab9f8e3a2d2edb1105b81d
musabhc <mbhc4235@gmail.com>
2021-01-16 23:42:22 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BaslangicCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double ornekSayi = 5.2;
+            bool sistemKontrol = false;
+
+            if (sistemKontrol == true)
+            {
+                Console.WriteLine("Giriş Başarılı!");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Başarısız!");
+            }
+        }
+    }
+}
